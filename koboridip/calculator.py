768fe1111db29c315868cad7a6f1f8982d927b15
Akira Kobori <private.beats@gmail.com>
2020-08-15 11:48:42 +0900

    Printerを型ヒントに追加

--- koboridip/calculator.py
+++ koboridip/calculator.py
@@ -1,8 +1,14 @@
 from abc import ABCMeta, abstractmethod
 
 
+class Printer(metaclass=ABCMeta):
+    @abstractmethod
+    def print(self, add, subtract, multiply, divide):
+        pass
+
+
 class Calculator():
-    def __init__(self, a: int, b: int, printer) -> None:
+    def __init__(self, a: int, b: int, printer: Printer) -> None:
         self.a = a
         self.b = b
         self.printer = printer
@@ -13,9 +19,3 @@ class Calculator():
         multiply = self.a * self.b
         divide = self.a / self.b
         self.printer.print(add, subtract, multiply, divide)
-
-
-class Printer(metaclass=ABCMeta):
-    @abstractmethod
-    def print(self, add, subtract, multiply, divide):
-        pass
